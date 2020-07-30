e3cc240f8e2a8e97b95874d3a9be025ddcb9cba3
Daniel Osypenko (Partner) <Daniel.Osypenko@motorolasolutions.com>
2020-07-30 11:06:34 +0300

    checkbuttons

--- src/App.js
+++ src/App.js
@@ -12,6 +12,42 @@ function App() {
                 {/*<div className="g-signin2" data-onsuccess="onSignIn"/>*/}
             </header>
             <RequestRegister className = 'checkButton'/>
+            <button value='res-get' onClick={fetch('https://safe-zone-il.herokuapp.com/res', {
+                method: 'GET',
+                name:'res-get',
+                headers : {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Cache': 'no-cache',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': '*'
+                },
+                credentials: 'include'
+            }).then(response => response.json())
+                .then((user) => {
+                    console.log(user.toString())
+                }).catch((error) => {
+                    console.error(error)
+                })}>res-get</button>
+            <button onClick={fetch('https://safe-zone-il.herokuapp.com/res-pos', {
+                method: 'POST',
+                name:'res-post',
+                headers : {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Cache': 'no-cache',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Headers': '*'
+                },
+                credentials: 'include'
+            }).then(response => response.json())
+                .then((user) => {
+                    console.log(user.toString())
+                }).catch((error) => {
+                    console.error(error)
+                })}>res-post</button>
         </div>
     );
 }
